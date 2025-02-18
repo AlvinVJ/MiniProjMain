0e82044882161204117fcff83e803b63ceece1d6
AlvinVJ <163581592+AlvinVJ@users.noreply.github.com>
2025-02-18 12:48:10 +0530

    initial commit

---
+++ trainer.py
@@ -0,0 +1,10 @@
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import cv2
+import os
+import numpy as np
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.optimizers import RMSprop
+
+print("hi")
